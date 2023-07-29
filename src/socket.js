76ce1f38bf6c68602c5dbb693316a09fd617baa6
Shomirsaidov Abubakr <115943155+Shomirsaidov@users.noreply.github.com>
2023-07-29 09:27:46 +0300

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -12,7 +12,7 @@ export const state = reactive({
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = process.env.NODE_ENV === "production" ? undefined : "http://localhost:3000";
 
-export const socket = io(URL, {autoConnect: true});
+export const socket = io("https://salom0server.vercel.app", {autoConnect: true});
 
 socket.on("connect", () => {
   state.connected = true;
